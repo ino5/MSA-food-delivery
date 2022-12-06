44cd97d407e703071466931f7e097b5f41cdc9ad
ino5 <chero77@naver.com>
2022-12-06 05:40:07 +0000

    UpdateCouponCommand 클래스 추가

---
+++ coupon/src/main/java/mall/external/UpdateCouponCommand.java
@@ -0,0 +1,12 @@
+package mall.external;
+
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.Data;
+
+@Data
+public class UpdateCouponCommand {
+
+    private String couponId;
+}
